fc1f20f2306489acb13010e2013301aed3dc62a4
Desbos <alexandredsb.pro@gmail.com>
2021-12-21 17:41:15 +0100

    supp fichiers

--- map_tileset/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="grass" tilewidth="16" tileheight="16" tilecount="280" columns="20">
- <image source="grass.png" trans="000000" width="320" height="224"/>
-</tileset>
